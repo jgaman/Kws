d94238ccf2edb54210266c84963c9b7946f97c66
jgaman <gaman2404@gmail.com>
2022-03-05 23:47:15 +0100

    build payload

--- data_analys.py
+++ data_analys.py
@@ -3,7 +3,14 @@
 import pandas as pd
 from pytrends.request import TrendReq
 
-# hl = host language tz = timezone
+# hl => host language tz => timezone
 pytrends = TrendReq(hl='en-US', tz=360)
 
 # Build payload
+all_keywords = ["michelin", "pirelli", "goodyear", "hankook", "bridgestone"]
+
+# cat='438' => str => Roues et pneus wheels and tires search for it on the url defaul = 0,
+# geo='' => str => tous les pays
+
+pytrends.build_payload(
+    kw_list, cat='438', timeframe='today 5-y', geo='', gprop='')
