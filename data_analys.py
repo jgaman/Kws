3a76f5e35f811d06d04b8b35e7cb8e73656ae858
jgaman <gaman2404@gmail.com>
2022-03-05 22:48:23 +0100

    loading all request libraries

---
+++ data_analys.py
@@ -0,0 +1,9 @@
+# request lxml and pandas
+
+import pandas as pd
+from pytrends.request import TrendReq
+
+# hl = host language tz = timezone
+pytrends = TrendReq(hl='en-US', tz=360)
+
+# Build payload
