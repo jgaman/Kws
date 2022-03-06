5ff2addf9846f808d4c227afbb5849a14b7aabb6
jgaman <gaman2404@gmail.com>
2022-03-06 11:08:37 +0100

    params adjustement

--- data_analys.py
+++ data_analys.py
@@ -8,10 +8,11 @@ pytrends = TrendReq(hl='en-US', tz=360)
 
 # Build payload
 all_keywords = ["michelin", "pirelli", "goodyear", "hankook", "bridgestone"]
+timeframes = ["today 5-y", "today 12-m", "today 3-m", "today 1-m"]
 
 # cat='438' => str => Roues et pneus wheels and tires search for it on the url defaul = 0,
-# geo='' => str => tous les pays
-# timeframe can be today 1-y
+# geo='' => str => yuo
+# timeframe can be 'today 1-y'
 
 pytrends.build_payload(
     kw_list, cat='438', timeframe='today 5-y', geo='', gprop='')
