cb6136034e202f6b2e3ac982f3880b41cad8c3f4
jgaman <gaman2404@gmail.com>
2022-03-06 11:54:46 +0100

    geo

--- data_analys.py
+++ data_analys.py
@@ -10,8 +10,8 @@ pytrends = TrendReq(hl='en-US', tz=360)
 all_keywords = ["michelin", "pirelli", "goodyear", "hankook", "bridgestone"]
 timeframes = ["today 5-y", "today 12-m", "today 3-m", "today 1-m"]
 
-# cat='438' => str => Roues et pneus wheels and tires search for it on the url defaul = 0,
-# geo='' => str => yuo
+# cat='438' => str => Roues et pneus wheels and tires, search for it on the url defaul = 0,
+# geo='' => str => countries, default '' => world wilde
 # timeframe can be 'today 1-y'
 
 pytrends.build_payload(
